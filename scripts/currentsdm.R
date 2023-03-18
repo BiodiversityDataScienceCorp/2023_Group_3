1dc89cef3507cd0f4ddeef5d8b8928953ea9e938
BailieWynbelt <wynbeltb@arizona.edu>
2023-03-18 23:03:18 +0000

    add current sdm script

---
+++ scripts/currentsdm.R
@@ -0,0 +1,13 @@
+#Current SDM Model
+#3/18/20233
+#Synthesized from team member's individual scripts
+
+
+# SECTION 1: Download and load required packages
+
+install.packages("spocc")
+install.packages("tidyverse") 
+install.packages("readr")
+library(spocc)
+library(tidyverse)
+library(readr)
