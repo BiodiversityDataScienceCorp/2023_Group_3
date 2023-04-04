75da97fa241c1731293e5a763fe39f84e9f55856
BailieWynbelt <wynbeltb@arizona.edu>
2023-04-04 19:21:17 +0000

    add error documentation

---
+++ potential_errors.R
@@ -0,0 +1,13 @@
+#Potential Errors
+#04/04/2023
+#Bailie Wynbelt, Hailey Park, Zoe Evans, Josie Graydon
+
+#This file contains a list of potential errors that could happen when running the script
+
+### GGPLOT ERRORS ###
+
+##If ggplot is not working run the following before running the ggplot code for future and current sdm
+
+#dev.off() 
+#Use if ggplot is not working, otherwise this is not needed
+#This function closes the specified plot (by default the current device)
